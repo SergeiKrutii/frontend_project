59e6b2f5ddc3426f492a4d89c1d745209434155f
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2023-12-15 22:03:53 +0200

    validation and fix styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,15 +6,41 @@ import {
   StyledForm,
   StyledLabel,
   StyledInputWrapper,
+  StyledFormikError,
 } from "./StyledForm";
 import MainButton from "../common/mainButton/MainButton";
 import { useSignupMutation, useLoginMutation } from "redux/auth/authApiSlice";
+import * as Yup from "yup";
 
 const Form = ({ loc, btnText }) => {
   const isRegisterPage = loc === "/register";
   const [setRegisterData, { isSuccess }] = useSignupMutation();
   const [setLoginData] = useLoginMutation();
   const navigate = useNavigate();
+
+  const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+      .max(10, "Ім'я повинно бути не більше 10 символів, або менше")
+      .required("Обов'язкове поле"),
+    email: Yup.string()
+      .email("Не корректна почтова адресса")
+      .required("Обов'язкове поле"),
+    password: Yup.string()
+      .required("Введіть пароль")
+      .min(6, "Пароль має бути не меньше 6 символів")
+      .max(12, "Пароль має бути не більше 12 символів"),
+    equalPassword: Yup.string().oneOf(
+      [Yup.ref("password"), null],
+      "Пароль повинен співпадати"
+    ),
+  });
+
+  const SigninSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Не корректна почтова адресса")
+      .required("Обов'язкове поле"),
+    password: Yup.string().required("Введіть пароль"),
+  });
 
   const formik = useFormik({
     initialValues: {
@@ -23,6 +49,7 @@ const Form = ({ loc, btnText }) => {
       password: "",
       equalPassword: "",
     },
+    validationSchema: isRegisterPage ? SignupSchema : SigninSchema,
     onSubmit: (values) => {
       const loginData = { email: values.email, password: values.password };
       if (isRegisterPage) {
@@ -50,8 +77,12 @@ const Form = ({ loc, btnText }) => {
             name="name"
             type="text"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.name}
           />
+          {formik.touched.name && formik.errors.name ? (
+            <StyledFormikError>{formik.errors.name}</StyledFormikError>
+          ) : null}
         </StyledInputWrapper>
       )}
 
@@ -60,10 +91,14 @@ const Form = ({ loc, btnText }) => {
         <StyledInput
           id="email"
           name="email"
-          type="text"
+          type="email"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.email}
         />
+        {formik.touched.email && formik.errors.email ? (
+          <StyledFormikError>{formik.errors.email}</StyledFormikError>
+        ) : null}
       </StyledInputWrapper>
 
       <StyledInputWrapper>
@@ -73,8 +108,12 @@ const Form = ({ loc, btnText }) => {
           name="password"
           type="password"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.password}
         />
+        {formik.touched.password && formik.errors.password ? (
+          <StyledFormikError>{formik.errors.password}</StyledFormikError>
+        ) : null}
       </StyledInputWrapper>
       {isRegisterPage && (
         <StyledInputWrapper>
@@ -84,8 +123,12 @@ const Form = ({ loc, btnText }) => {
             name="equalPassword"
             type="password"
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             value={formik.values.equalPassword}
           />
+          {formik.touched.equalPassword && formik.errors.equalPassword ? (
+            <StyledFormikError>{formik.errors.equalPassword}</StyledFormikError>
+          ) : null}
         </StyledInputWrapper>
       )}
 
--- src/pages/libraryPage/LibraryPage.jsx
+++ src/pages/libraryPage/LibraryPage.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { useMatchMedia } from "helpers/mediaQuery";
 
 import CategoryLibrary from "components/common/сategoryLibrary/CategoryLibrary";
@@ -8,45 +8,65 @@ import { StyledLibralyPage } from "./StyledLibralyPage";
 import LinkPageAdd from "components/common/LinkPageAdd";
 import FormAddBook from "components/common/formAddBook/FormAddBook";
 import EmptySteps from "components/common/EmptySteps";
+import { useGetUserQuery } from "redux/auth/authApiSlice";
 
 const LibraryPage = () => {
   const [books, setBooks] = useState(null || []);
-  const [isReadBooks, setIsReadBooks] = useState([])
-  const [isWantReadToBooks, setWantReadToBooks] = useState([])
-  const [isReadingBooks, setIsReadingBooks] = useState([])
-  const {isMobile, isTablet, isDesktop} = useMatchMedia()
+  const [isReadBooks, setIsReadBooks] = useState([]);
+  const [isWantReadToBooks, setWantReadToBooks] = useState([]);
+  const [isReadingBooks, setIsReadingBooks] = useState([]);
+  const { isMobile, isTablet, isDesktop } = useMatchMedia();
 
-    useEffect(() => {
-        import('./books.json')
-            .then((data) => { setBooks(data.books) })
-            .catch((error) => { console.error('Ошибка') });
-    }, [])
+  const { data } = useGetUserQuery();
+
+  useEffect(() => {
+    import("./books.json")
+      .then((data) => {
+        setBooks(data.books);
+      })
+      .catch((error) => {
+        console.error("Ошибка");
+      });
+  }, []);
 
   useEffect(() => {
     if (books) {
-      const readingBooks = books.filter((book) => book.rating === "Вже прочитано");
+      const readingBooks = books.filter(
+        (book) => book.rating === "Вже прочитано"
+      );
       setIsReadingBooks(readingBooks);
-      const readBooks = books.filter((book) => book.rating === "Читаю")
+      const readBooks = books.filter((book) => book.rating === "Читаю");
       setIsReadBooks(readBooks);
-      const wantRead = books.filter((book) => book.rating === "Маю намір прочитати")
+      const wantRead = books.filter(
+        (book) => book.rating === "Маю намір прочитати"
+      );
       setWantReadToBooks(wantRead);
     }
   }, [books]);
 
-  return isMobile ? (<StyledLibralyPage>
-    <CategoryLibrary
-      isReadingBooks={isReadingBooks}
-      isReadBooks={isReadBooks}
-      isWantReadToBooks={isWantReadToBooks} />
-    <LinkPageAdd />
-  </StyledLibralyPage>)
-    : (<StyledLibralyPage>
+  return isMobile ? (
+    <StyledLibralyPage>
+      <CategoryLibrary
+        isReadingBooks={isReadingBooks}
+        isReadBooks={isReadBooks}
+        isWantReadToBooks={isWantReadToBooks}
+      />
+      <LinkPageAdd />
+    </StyledLibralyPage>
+  ) : (
+    <StyledLibralyPage>
       <FormAddBook />
-      {books ? (<CategoryLibrary
-      isReadingBooks={isReadingBooks}
-      isReadBooks={isReadBooks}
-      isWantReadToBooks={isWantReadToBooks} />):(<EmptySteps />)}
-  </StyledLibralyPage>);
+      {books ? (
+        <CategoryLibrary
+          isReadingBooks={isReadingBooks}
+          isReadBooks={isReadBooks}
+          isWantReadToBooks={isWantReadToBooks}
+        />
+      ) : (
+        <EmptySteps />
+      )}
+    </StyledLibralyPage>
+  );
 };
 
 LibraryPage.propTypes = {};
